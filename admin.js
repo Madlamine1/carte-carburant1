74cec740eecf4c21ecff80dc49fdbb5686207dba
Madlamine1 <107039291+Madlamine1@users.noreply.github.com>
2026-09-12 14:40:52 +0000

    Add files via upload

---
+++ admin.js
@@ -0,0 +1,34 @@
+const ADMIN_DATA = [
+
+    {
+        code: "01",
+        name: "Kayes",
+        type: "region",
+
+        cercles: [
+
+            {
+                code: "0101",
+                name: "Kayes",
+
+                communes: [
+                    {
+                        code: "01010101",
+                        name: "Kayes"
+                    },
+
+                    {
+                        code: "01010102",
+                        name: "Bangassi"
+                    }
+
+                    // ...
+                ]
+            }
+
+            // ...
+        ]
+    }
+
+    // ...
+];
